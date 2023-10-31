cd32b81bc9a688c6f6f05c26d4c169b8befd4b8e
Priyansh Jain <quic_priyjain@quicinc.com>
2023-10-31 16:04:32 +0530

    thermal-hal: Enable support for lemans auto target
    Enable support for thermal hal for lemans auto chipset.
    
    Change-Id: Icb8d8b65961c999159bd0fe90abdef07bc2e8798
    Signed-off-by: Priyansh Jain <quic_priyjain@quicinc.com>

--- thermalConfig.cpp
+++ thermalConfig.cpp
@@ -1515,6 +1515,69 @@ namespace thermal {
 		},
 	};
 
+	std::vector<std::string> cpu_sensors_lemansAU =
+	{
+		"cpu-0-0-0",
+		"cpu-0-1-0",
+		"cpu-0-2-0",
+		"cpu-0-3-0",
+		"cpu-1-0-0",
+		"cpu-1-1-0",
+		"cpu-1-2-0",
+		"cpu-1-3-0",
+	};
+
+	std::vector<struct target_therm_cfg>  lemansAU_common = {
+		{
+			TemperatureType::CPU,
+			cpu_sensors_lemansAU,
+			"",
+			105000,
+			118000,
+			true,
+		},
+		{
+			TemperatureType::GPU,
+			{ "gpuss-0" },
+			"GPU0",
+			105000,
+			118000,
+			true,
+		},
+		{
+			TemperatureType::GPU,
+			{ "gpuss-1" },
+			"GPU1",
+			105000,
+			118000,
+			true,
+		},
+		{
+			TemperatureType::GPU,
+			{ "gpuss-2" },
+			"GPU2",
+			105000,
+			118000,
+			true,
+		},
+		{
+			TemperatureType::NPU,
+			{ "nsp-0-0-0" },
+			"nsp0",
+			105000,
+			118000,
+			true,
+		},
+		{
+			TemperatureType::NPU,
+			{ "nsp-1-0-0" },
+			"nsp1",
+			105000,
+			118000,
+			true,
+		},
+	};
+
 	struct target_therm_cfg bat_conf = {
 		TemperatureType::BATTERY,
 		{ "battery" },
@@ -2390,6 +2453,7 @@ std::vector<std::string> cpu_sensors_cliffs = {
 		{582, ravelin_common}, //Clarence IOT without modem
 		{591, waipio_common}, //ukee
 		{608, crow_common}, //crow
+		{532, lemansAU_common}, //Lemans auto
 	};
 
 	const std::unordered_map<int, std::vector<struct target_therm_cfg>>
